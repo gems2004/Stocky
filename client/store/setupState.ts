e97dd6bc906039627fb64ab9c0a8987468b5cde7
Edmon <ed.moussa4@gmail.com>
2025-09-01 17:19:28 +0300

    Created stores

---
+++ client/store/setupState.ts
@@ -0,0 +1,39 @@
+import { DatabaseConfigDto, SetupStatusDto, ShopInfoDto } from "@/(api)/type";
+import { create } from "zustand";
+
+type Admin = {
+  username: string;
+  email: string;
+  firstName: string;
+  lastName: string | undefined;
+};
+
+interface SetupStore {
+  shopInfo: ShopInfoDto | null;
+  databaseConfig: DatabaseConfigDto | null;
+  user: Admin | null;
+  setShopInfo: (shopInfo: ShopInfoDto) => void;
+  setDatabaseConfig: (databaseConfig: DatabaseConfigDto) => void;
+  setUser: (user: Admin) => void;
+
+  setupStatus: SetupStatusDto;
+  setSetupStatus: (status: SetupStatusDto) => void;
+}
+
+export const useSetupStore = create<SetupStore>((set) => ({
+  shopInfo: null,
+  databaseConfig: null,
+  user: null,
+  setShopInfo: (shopInfo) => set({ shopInfo: shopInfo }),
+  setDatabaseConfig: (databaseConfig) =>
+    set({ databaseConfig: databaseConfig }),
+  setUser: (user) => set({ user: user }),
+  clearUser: () => set({ user: null }),
+
+  setupStatus: {
+    isAdminUserCreated: false,
+    isDatabaseConfigured: false,
+    isShopInfoSet: false,
+  },
+  setSetupStatus: (status) => set({ setupStatus: status }),
+}));
---
+++ client/store/userStore.ts
@@ -0,0 +1,24 @@
+import { AdminUserDto, DatabaseConfigDto, ShopInfoDto } from "@/(api)/type";
+import { create } from "zustand";
+
+type User = {
+  username: string;
+  email: string;
+  role: "Admin" | "Employee";
+  firstName: string;
+  lastName: string | undefined;
+};
+
+interface UserState {
+  user: User | null;
+  setUser: (user: User) => void;
+  getCurrentUser: () => User | null;
+  clearUser: () => void;
+}
+
+export const useUserStore = create<UserState>((set, get) => ({
+  user: null,
+  setUser: (user) => set({ user: user }),
+  getCurrentUser: () => get().user,
+  clearUser: () => set({ user: null }),
+}));
