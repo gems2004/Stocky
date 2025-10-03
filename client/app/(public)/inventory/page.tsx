c2ca118c186d654fb2f895b7b3950c9bfa13fd20
Edmon <ed.moussa4@gmail.com>
2025-10-04 00:05:08 +0300

    Added create product button

--- client/app/(public)/inventory/page.tsx
+++ client/app/(public)/inventory/page.tsx
@@ -2,12 +2,21 @@ import H3 from "@/components/typography/H3";
 
 import React from "react";
 import InventoryTable from "./inventoryTable";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Inventory() {
   return (
-    <>
-      <H3 className="py-4">Store Inventory:</H3>
-      <InventoryTable />
-    </>
+    <div className="flex flex-col h-full justify-between">
+      <div>
+        <H3 className="py-4">Store Inventory:</H3>
+        <InventoryTable />
+      </div>
+      <Button asChild>
+        <Link href="/products/new" className="self-end">
+          Create Product
+        </Link>
+      </Button>
+    </div>
   );
 }
