6c313d22e44a36f21fde8ddebaf3581797c6d4ee
Edmon <ed.moussa4@gmail.com>
2025-10-04 00:09:52 +0300

    Implemented getProducts

--- client/app/(public)/inventory/inventoryTable.tsx
+++ client/app/(public)/inventory/inventoryTable.tsx
@@ -10,35 +10,52 @@ import {
 } from "@/components/ui/table";
 import { Pen } from "lucide-react";
 import { useGetProducts } from "@/api/productsApi";
+import { ProductResponseDto } from "@/api/type";
+
 export default function InventoryTable() {
-  const { data: products } = useGetProducts();
-  console.log(products);
+  const { data: response, isLoading, isSuccess } = useGetProducts();
+
+  if (isLoading) return <p>Loading...</p>;
+
+  if (!isSuccess) return;
+
+  const products: ProductResponseDto[] = response?.success
+    ? response.data.data
+    : [];
 
   return (
-    <Table className="w-full">
+    <Table className="w-full grow flex-1">
       <TableHeader>
         <TableRow>
           <TableHead>Product</TableHead>
           <TableHead>SKU</TableHead>
           <TableHead>Supplier</TableHead>
+          <TableHead>Category</TableHead>
           <TableHead>In Stock</TableHead>
           <TableHead>Order At</TableHead>
           <TableHead>Price</TableHead>
+          <TableHead>Cost</TableHead>
           <TableHead>Action</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
-        <TableRow>
-          <TableCell>Test</TableCell>
-          <TableCell>T41</TableCell>
-          <TableCell>Burger King</TableCell>
-          <TableCell>13</TableCell>
-          <TableCell>10</TableCell>
-          <TableCell>$15</TableCell>
-          <TableCell>
-            <Pen />
-          </TableCell>
-        </TableRow>
+        {products.map((product) => {
+          return (
+            <TableRow key={product.id}>
+              <TableCell>{product.name}</TableCell>
+              <TableCell>{product.sku}</TableCell>
+              <TableCell>{product.supplier.name}</TableCell>
+              <TableCell>{product.category.name}</TableCell>
+              <TableCell>{product.stockQuantity}</TableCell>
+              <TableCell>{product.minStockLevel}</TableCell>
+              <TableCell>{product.price}</TableCell>
+              <TableCell>{product.cost}</TableCell>
+              <TableCell>
+                <Pen />
+              </TableCell>
+            </TableRow>
+          );
+        })}
       </TableBody>
     </Table>
   );
