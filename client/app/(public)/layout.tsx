567be45268bf642a87435ba3132b9e8c446d917e
Edmon <ed.moussa4@gmail.com>
2025-10-04 12:31:07 +0300

    Added redirect if store is not setup

--- client/app/(public)/layout.tsx
+++ client/app/(public)/layout.tsx
@@ -1,13 +1,24 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import AppSidebar from "@/layout/AppSidebar";
+import { useGetSetupStatus } from "@/api/setupApi";
+import { useRouter } from "next/navigation";
 
 export default function PublicLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const { data: status, isSuccess } = useGetSetupStatus();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status?.success && !status.data.isSetupComplete) {
+      router.push("/setup");
+    }
+  }, [isSuccess]);
+
   return (
     <SidebarProvider>
       <AppSidebar />
